4db798cd1a3f8053e43e1b5648c160280053f0f3
= <=>
2023-05-12 14:10:03 -0700

    testing

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
-import os
+
 app = Flask(__name__)
 
-app.secret_key = os.urandom(12)
 
 @app.route("/")
 def hello():
-    return f"Hello, World - DataDemo!{app.secrety_key}"
+    return f"Hello, World - DataDemo!"
